983577a21dd3dcd93295ca6e742b6ab572135e08
jonghwann <jonghwan0729@gmail.com>
2026-02-02 23:37:01 +0900

    chore: biome 기본 설정 추가

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,8 +1,8 @@
 import { AppsInToss } from '@apps-in-toss/framework';
 import type { InitialProps } from '@granite-js/react-native';
+import { TDSProvider } from '@toss/tds-react-native';
 import type { PropsWithChildren } from 'react';
 import { context } from '../require.context';
-import { TDSProvider } from '@toss/tds-react-native';
 
 function AppContainer({ children }: PropsWithChildren<InitialProps>) {
   return <TDSProvider>{children}</TDSProvider>;
